e25f964ed832b6c543c63482c1102ef84bd45a2f
MMortari <30581082+MMortari@users.noreply.github.com>
2019-03-18 13:29:42 -0300

    Update watson.js

--- backend/src/configs/watson.js
+++ backend/src/configs/watson.js
@@ -1,4 +1,4 @@
 module.exports = {
-  iam_apikey: "1emJ3Qxf-k7k3zOQLKOv7KZZpNwUbrf7UWSXh5f6E7g2",
-  url: "https://stream.watsonplatform.net/text-to-speech/api"
+  iam_apikey: "YOUR_APIKEY",
+  url: "YOUR_URL"
 };
